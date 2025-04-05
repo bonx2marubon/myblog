334b893f03de7e3e14ba82f0892074199ccc9b3e
bonx2marubon <mamamarurururumarumaru@gmail.com>
2025-04-05 21:41:54 +0900

    reset.cssの追加とstyle.cssの準備

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -1,45 +1,45 @@
 package com.example.demo.model;
 
-import org.sprigframework.stereotype.Component;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
 @Component
 @SessionScope
 public class Account {
-  private String name;
-  private String email;
-  private String password;
+	private String name;
+	private String email;
+	private String password;
 
-  public Account(){
-  }
+	public Account() {
+	}
 
-  public Account(String name, String email, String password){
-    this.name = name;
-    this.email = email;
-    this.password = password;
-  }
+	public Account(String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
 
-  public String getName(){
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name){
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public String getEmail(){
-    return email;
-  }
+	public String getEmail() {
+		return email;
+	}
 
-  public void setEmail(String email){
-    this.email = email;
-  }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-  public String getPassword(){
-    return password;
-  }
+	public String getPassword() {
+		return password;
+	}
 
-  public void setPassword(String password){
-    this.password = password;
-  }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
