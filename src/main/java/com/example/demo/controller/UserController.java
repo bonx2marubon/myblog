17d15bb4d1301ffa047982ecf5686a421d3c8fc1
bonx2marubon <mamamarurururumarumaru@gmail.com>
2025-04-05 17:55:49 +0900

    UserControllerに新規登録とログインの動作の記述を追加

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.entity.User;
+import com.example.demo.model.Account;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -18,7 +18,7 @@ public class UserController {
 	HttpSession session;
 
 	@Autowired
-	User user;
+	Account account;
 
 	// ログイン画面を表示
 	@GetMapping({ "/", "/login", "/logout" })
@@ -27,6 +27,10 @@ public class UserController {
 			Model model) {
 		// セッション情報を全てクリアする
 		session.invalidate();
+		// エラーパラメータのチェック
+		if (error.equals("notLoggedIn")) {
+			model.addAttribute("message", "メールアドレスとパスワードを入力してください");
+		}
 		return "login";
 	}
 
@@ -39,8 +43,36 @@ public class UserController {
 		// 名前が空の場合やパスワードがからの場合にエラーとする
 		if (email == null || password == null) {
 			model.addAttribute("message", "メールアドレスとパスワードを入力してください");
-			return "login";
 		}
+		return "login";
 	}
 
+	// 新規登録画面を表示
+	@GetMapping("/new")
+	public String signup(
+			@RequestParam(name = "error", defaultValue = "") String error,
+			Model model) {
+		// セッション情報を全てクリアする
+		session.invalidate();
+		// エラーパラメータのチェック
+		if (error.equals("notLoggedIn")) {
+			model.addAttribute("message", "全ての項目を入力してください");
+		}
+		return "signup";
+	}
+
+	// 新規登録の実行
+	@PostMapping("/new")
+	public String create(
+			@RequestParam("name") String name,
+			@RequestParam("email") String email,
+			@RequestParam("password") String password,
+			Model model) {
+		// パラメータが空文字の場合はエラーメッセージを出力
+		if (name == null || name.length() == 0 || email == null || email.length() == 0 || password == null
+				|| password.length() == 0) {
+			model.addAttribute("message", "名前、メールアドレスとパスワードを入力してください");
+		}
+		return "signup";
+	}
 }
