31f2fe4feb67472c537befae939492ae800c1451
bonx2marubon <mamamarurururumarumaru@gmail.com>
2025-04-12 15:17:10 +0900

    新規登録用にパスワード（確認用）の記述を追加

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -9,14 +9,16 @@ public class Account {
 	private String name;
 	private String email;
 	private String password;
+	private String confirmPassword;
 
 	public Account() {
 	}
 
-	public Account(String name, String email, String password) {
+	public Account(String name, String email, String password, String confirmPassword) {
 		this.name = name;
 		this.email = email;
 		this.password = password;
+		this.confirmPassword = confirmPassword;
 	}
 
 	public String getName() {
@@ -41,5 +43,13 @@ public class Account {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
 	}
 }
