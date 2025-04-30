d3ac62905fe81698672529f8c65f4ee51b50f8b0
bonx2marubon <mamamarurururumarumaru@gmail.com>
2025-04-30 23:51:26 +0900

    BlogとCategoryクラスの記述の見直し

--- src/main/java/com/example/demo/entity/Blog.java
+++ src/main/java/com/example/demo/entity/Blog.java
@@ -1,5 +1,55 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "blogs")
 public class Blog {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name = "category_id")
+	private Integer categoryId;
+	@Column(name = "user_id")
+	private Integer userId;
+	private String title;
+	private String body;
+
+	// コンストラクタ
+	public Blog() {
+	}
+
+	public Blog(Integer categoryId, String title, String body) {
+		this.categoryId = categoryId;
+		this.title = title;
+		this.body = body;
+	}
+
+	// ゲッター
+	public Integer getId() {
+		return id;
+	}
+
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getBody() {
+		return body;
+	}
 
 }
